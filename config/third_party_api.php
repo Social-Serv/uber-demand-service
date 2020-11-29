d000ac91ce99e194152b0d720f739725b8134009
Yehor <C:\Users\user\AppData\Roaming\The Bat!>
2020-11-29 12:08:07 +0200

    routes configured

--- config/third_party_api.php
+++ config/third_party_api.php
@@ -2,7 +2,7 @@
 // https://proxy-service/client/request-trip?forward="TripManager"
 return [
     'proxy' => [
-        'host' => '/',
+        'host' => 'https://uber-proxy.herokuapp.com/',
     ],
     'trip_mngr' => [
         'name' => 'trip-mgr',
@@ -16,16 +16,16 @@ return [
         'name' => 'storage',
         'proxy_param' => '?forward=storage',
         'urls' => [
-            'get_driver' => '/',
-            'get_rider' => '/',
-            'get_car_classes' => '/',
-            'get_trip_info' => '/',
+            'get_driver' => 'api/front/car/',
+            'get_rider' => 'api/front/rider/',
+            'get_car_classes' => 'api/car_classes',
+            'get_trip_info' => 'api/services/trip/',
             'get_riders_trips' => [
-                'prefix' => '/',
-                'postfix' => '/'
+                'prefix' => 'api/front/rider/',
+                'postfix' => '/trips'
             ],
-            'create_rider' => '/',
-            'update_rider' => '/',
+            'create_rider' => 'api/front/rider',
+            'update_rider' => 'api/front/rider/',
         ]
     ]
 ];
